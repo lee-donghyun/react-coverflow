097150216e2ec21a39663be5712154e34e432e9a
Donghyun Lee <leedonghyun022@gmail.com>
2025-05-25 22:22:32 +0900

    feat: 문서화 개선

--- src/coverflow/util.ts
+++ src/coverflow/util.ts
@@ -1,38 +1,67 @@
 export class Util {
-  private firstGap: number;
-  private gap: number;
-  private firstScale: number;
-  private scale: number;
+  private xWeightRegion1: number;
+  private xWeightRegion2: number;
+  private scaleWeightRegion1: number;
+  private scaleWeightRegion2: number;
   private rubber: number;
 
   constructor(size: number) {
-    this.firstGap = (size * 220) / 400;
-    this.gap = (size * 80) / 400;
-    this.firstScale = -0.2;
-    this.scale = -0.05;
+    this.xWeightRegion1 = size * 0.55;
+    this.xWeightRegion2 = size * 0.2;
+    this.scaleWeightRegion1 = -0.2;
+    this.scaleWeightRegion2 = -0.05;
     this.rubber = 0.15;
   }
 
+  /**
+   * get the x position of the cover based on its score
+   * ```
+   *     x                   x
+   *     │                   │
+   *   0 ┼─────────x         │
+   *     ┼─────────┼─────────┼─
+   *    -1         0         1
+   * ```
+   * @param score - the score of the cover, which is the index of the cover in the array
+   * @returns
+   */
   getX(score: number) {
     if (score < -1) {
-      return -this.firstGap + this.gap * (score + 1);
+      return -this.xWeightRegion1 + this.xWeightRegion2 * (score + 1);
     }
     if (score < 1) {
-      return score * this.firstGap;
+      return score * this.xWeightRegion1;
     }
-    return this.firstGap + this.gap * (score - 1);
+    return this.xWeightRegion1 + this.xWeightRegion2 * (score - 1);
   }
 
+  /**
+   * inverse of getX
+   * @param x - the x position
+   * @returns score
+   */
   getScore(x: number) {
-    if (x < -this.firstGap) {
-      return (x + this.firstGap) / this.gap - 1;
+    if (x < -this.xWeightRegion1) {
+      return (x + this.xWeightRegion1) / this.xWeightRegion2 - 1;
     }
-    if (x < this.firstGap) {
-      return x / this.firstGap;
+    if (x < this.xWeightRegion1) {
+      return x / this.xWeightRegion1;
     }
-    return (x - this.firstGap) / this.gap + 1;
+    return (x - this.xWeightRegion1) / this.xWeightRegion2 + 1;
   }
 
+  /**
+   * get the rotateY of the cover based on its score
+   * ```
+   *   0 ┼─────────x
+   *     │         │
+   * -40 x         │         x
+   *    ─┼─────────┼─────────┼─
+   *    -1         0         1
+   * ```
+   * @param score - the score of the cover, which is the index of the cover in the array
+   * @returns
+   */
   getRotateY(score: number) {
     if (score < -1) {
       return 40;
@@ -43,23 +72,41 @@ export class Util {
     return -40;
   }
 
+  /**
+   * get thre scale of the cover based on its score
+   * ```
+   *    1 ┬─────────x
+   *      │         │
+   *  0.8 ┼────x    │    x
+   *      │    │    │    │
+   * 0.75 x    │    │    │    x
+   *     ─┼────┼────┼────┼────┼─
+   *     -2   -1    0    1    2
+   * ```
+   * @param score - the score of the cover, which is the index of the cover in the array
+   * @returns
+   */
   getScale(score: number) {
     if (score < -2) {
-      return 1 + this.firstScale + this.scale;
+      return 1 + this.scaleWeightRegion1 + this.scaleWeightRegion2;
     }
     if (score < -1) {
-      return 1 + this.firstScale - this.scale * (score + 1);
+      return (
+        1 + this.scaleWeightRegion1 - this.scaleWeightRegion2 * (score + 1)
+      );
     }
     if (score < 0) {
-      return 1 - this.firstScale * score;
+      return 1 - this.scaleWeightRegion1 * score;
     }
     if (score < 1) {
-      return 1 + this.firstScale * score;
+      return 1 + this.scaleWeightRegion1 * score;
     }
     if (score < 2) {
-      return 1 + this.firstScale + this.scale * (score - 1);
+      return (
+        1 + this.scaleWeightRegion1 + this.scaleWeightRegion2 * (score - 1)
+      );
     }
-    return 1 + this.firstScale + this.scale;
+    return 1 + this.scaleWeightRegion1 + this.scaleWeightRegion2;
   }
 
   getTransform(score: number) {
@@ -70,6 +117,13 @@ export class Util {
     };
   }
 
+  /**
+   * multiply the x position by a rubber band effect
+   * @param baseX
+   * @param x
+   * @param size
+   * @returns the bounded x position`
+   */
   getBoundedX(baseX: number, x: number, size: number) {
     const offset = baseX + x;
     const x0 = -this.getX(0);
