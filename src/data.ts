4b43c674e1f3c958c5f8bfaf910d2c220214d53e
Donghyun Lee <leedonghyun022@gmail.com>
2025-08-15 20:56:49 +0900

    feat: 데이터 추가

--- src/data.ts
+++ src/data.ts
@@ -1,7 +1,869 @@
 export const covers = [
   {
-    src: "1.jpg",
-    title: "Cover 1",
-    tracks: [{ title: "Track 1-1" }],
+    title: "eternal sunshine deluxe: brighter days ahead (a cappella version)",
+    tracks: [
+      {
+        title: "intro (end of the world)",
+      },
+      {
+        title: "bye",
+      },
+      {
+        title: "don't wanna break up again",
+      },
+      {
+        title: "Saturn Returns Interlude",
+      },
+      {
+        title: "eternal sunshine",
+      },
+      {
+        title: "supernatural",
+      },
+      {
+        title: "true story",
+      },
+      {
+        title: "the boy is mine",
+      },
+      {
+        title: "yes, and?",
+      },
+      {
+        title: "we can't be friends (wait for your love)",
+      },
+      {
+        title: "i wish i hated you",
+      },
+      {
+        title: "imperfect for you",
+      },
+      {
+        title: "ordinary things (feat. Nonna)",
+      },
+      {
+        title: "intro (end of the world) (extended)",
+      },
+      {
+        title: "twilight zone",
+      },
+      {
+        title: "warm",
+      },
+      {
+        title: "dandelion",
+      },
+      {
+        title: "past life",
+      },
+      {
+        title: "Hampstead",
+      },
+      {
+        title: "intro (end of the world) (a cappella)",
+      },
+      {
+        title: "bye (a cappella)",
+      },
+      {
+        title: "don't wanna break up again (a cappella)",
+      },
+      {
+        title: "Saturn Returns Interlude (a cappella)",
+      },
+      {
+        title: "eternal sunshine (a cappella)",
+      },
+      {
+        title: "supernatural (a cappella)",
+      },
+      {
+        title: "true story (a cappella)",
+      },
+      {
+        title: "the boy is mine (a cappella)",
+      },
+      {
+        title: "yes, and? (a cappella)",
+      },
+      {
+        title: "we can't be friends (wait for your love) (a cappella)",
+      },
+      {
+        title: "i wish i hated you (a cappella)",
+      },
+      {
+        title: "imperfect for you (a cappella)",
+      },
+      {
+        title: "ordinary things (a cappella) (feat. Nonna)",
+      },
+      {
+        title: "intro (end of the world) (extended a cappella)",
+      },
+      {
+        title: "twilight zone (a cappella)",
+      },
+      {
+        title: "warm (a cappella)",
+      },
+      {
+        title: "dandelion (a cappella)",
+      },
+      {
+        title: "past life (a cappella)",
+      },
+      {
+        title: "Hampstead (a cappella)",
+      },
+    ],
+    src: "1.png",
+  },
+  {
+    title: "F-1 Trillion",
+    tracks: [
+      {
+        title: "Wrong Ones (feat. Tim McGraw)",
+      },
+      {
+        title: "Finer Things (feat. Hank Williams Jr.)",
+      },
+      {
+        title: "I Had Some Help (feat. Morgan Wallen)",
+      },
+      {
+        title: "Pour Me A Drink (feat. Blake Shelton)",
+      },
+      {
+        title: "Have The Heart (feat. Dolly Parton)",
+      },
+      {
+        title: "What Don't Belong To Me",
+      },
+      {
+        title: "Goes Without Saying (feat. Brad Paisley)",
+      },
+      {
+        title: "Guy For That (feat. Luke Combs)",
+      },
+      {
+        title: "Nosedive (feat. Lainey Wilson)",
+      },
+      {
+        title: "Losers (feat. Jelly Roll)",
+      },
+      {
+        title: "Devil I've Been (feat. ERNEST)",
+      },
+      {
+        title: "Never Love You Again (feat. Sierra Ferrell)",
+      },
+      {
+        title: "Missin’ You Like This (feat. Luke Combs)",
+      },
+      {
+        title: "California Sober (feat. Chris Stapleton)",
+      },
+      {
+        title: "Hide My Gun (feat. HARDY)",
+      },
+      {
+        title: "Right About You",
+      },
+      {
+        title: "M-E-X-I-C-O (feat. Billy Strings)",
+      },
+      {
+        title: "Yours",
+      },
+    ],
+    src: "F-1 Trillion.png",
+  },
+  {
+    title: "Fireworks & Rollerblades",
+    tracks: [
+      {
+        title: "Intro",
+      },
+      {
+        title: "Be Someone",
+      },
+      {
+        title: "Slow It Down",
+      },
+      {
+        title: "Beautiful Things",
+      },
+      {
+        title: "Cry",
+      },
+      {
+        title: "Forever and a Day",
+      },
+      {
+        title: "In The Stars",
+      },
+      {
+        title: "Drunk In My Mind",
+      },
+      {
+        title: "My Greatest Fear",
+      },
+      {
+        title: "There She Goes",
+      },
+      {
+        title: "Hello Love",
+      },
+      {
+        title: "Ghost Town",
+      },
+      {
+        title: "Love Of Mine",
+      },
+      {
+        title: "Friend",
+      },
+      {
+        title: "What Do You Want",
+      },
+    ],
+    src: "Fireworks & Rollerblades.png",
+  },
+  {
+    title: "HIT ME HARD AND SOFT",
+    tracks: [
+      {
+        title: "SKINNY",
+      },
+      {
+        title: "LUNCH",
+      },
+      {
+        title: "CHIHIRO",
+      },
+      {
+        title: "BIRDS OF A FEATHER",
+      },
+      {
+        title: "WILDFLOWER",
+      },
+      {
+        title: "THE GREATEST",
+      },
+      {
+        title: "L’AMOUR DE MA VIE",
+      },
+      {
+        title: "THE DINER",
+      },
+      {
+        title: "BITTERSUITE",
+      },
+      {
+        title: "BLUE",
+      },
+    ],
+    src: "HIT ME HARD AND SOFT.png",
+  },
+  {
+    title: "Hurry Up Tomorrow",
+    tracks: [
+      {
+        title: "Wake Me Up",
+      },
+      {
+        title: "Cry For Me",
+      },
+      {
+        title: "I Can't Fucking Sing",
+      },
+      {
+        title: "São Paulo",
+      },
+      {
+        title: "Until We're Skin & Bones",
+      },
+      {
+        title: "Baptized In Fear",
+      },
+      {
+        title: "Open Hearts",
+      },
+      {
+        title: "Opening Night",
+      },
+      {
+        title: "Reflections Laughing",
+      },
+      {
+        title: "Enjoy The Show",
+      },
+      {
+        title: "Given Up On Me",
+      },
+      {
+        title: "I Can't Wait To Get There",
+      },
+      {
+        title: "Timeless",
+      },
+      {
+        title: "Niagara Falls",
+      },
+      {
+        title: "Take Me Back To LA",
+      },
+      {
+        title: "Big Sleep",
+      },
+      {
+        title: "Give Me Mercy",
+      },
+      {
+        title: "Drive",
+      },
+      {
+        title: "The Abyss",
+      },
+      {
+        title: "Red Terror",
+      },
+      {
+        title: "Without a Warning",
+      },
+      {
+        title: "Hurry Up Tomorrow",
+      },
+    ],
+    src: "Hurry Up Tomorrow.png",
+  },
+  {
+    title: "I Said I Love You First",
+    tracks: [
+      {
+        title: "I Said I Love You First",
+      },
+      {
+        title: "Younger And Hotter Than Me",
+      },
+      {
+        title: "Call Me When You Break Up",
+      },
+      {
+        title: "Ojos Tristes",
+      },
+      {
+        title: "Don’t Wanna Cry",
+      },
+      {
+        title: "Sunset Blvd",
+      },
+      {
+        title: "Cowboy",
+      },
+      {
+        title: "Bluest Flame",
+      },
+      {
+        title: "How Does It Feel To Be Forgotten",
+      },
+      {
+        title: "Do You Wanna Be Perfect",
+      },
+      {
+        title: "You Said You Were Sorry",
+      },
+      {
+        title: "I Can’t Get Enough (feat. Tainy)",
+      },
+      {
+        title: "Don’t Take It Personally",
+      },
+      {
+        title: "Scared Of Loving You",
+      },
+    ],
+    src: "I Said I Love You First.png",
+  },
+  {
+    title: "I've Tried Everything But Therapy (Part 1)",
+    tracks: [
+      {
+        title: "Some Things I'll Never Know",
+      },
+      {
+        title: "Lose Control",
+      },
+      {
+        title: "What More Can I Say",
+      },
+      {
+        title: "The Door",
+      },
+      {
+        title: "Goodbye's Been Good to You",
+      },
+      {
+        title: "Last Communion",
+      },
+      {
+        title: "You Still Get to Me",
+      },
+      {
+        title: "Suitcase",
+      },
+      {
+        title: "Flame",
+      },
+      {
+        title: "Evergreen",
+      },
+    ],
+    src: "I've Tried Everything But Therapy (Part 1).png",
+  },
+  {
+    title: "MAYHEM",
+    tracks: [
+      {
+        title: "Disease",
+      },
+      {
+        title: "Abracadabra",
+      },
+      {
+        title: "Garden Of Eden",
+      },
+      {
+        title: "Perfect Celebrity",
+      },
+      {
+        title: "Vanish Into You",
+      },
+      {
+        title: "Killah",
+      },
+      {
+        title: "Zombieboy",
+      },
+      {
+        title: "LoveDrug",
+      },
+      {
+        title: "How Bad Do U Want Me",
+      },
+      {
+        title: "Don't Call Tonight",
+      },
+      {
+        title: "Shadow Of A Man",
+      },
+      {
+        title: "The Beast",
+      },
+      {
+        title: "Blade Of Grass",
+      },
+      {
+        title: "Die With A Smile",
+      },
+    ],
+    src: "MAYHEM.png",
+  },
+  {
+    title: "rosie",
+    tracks: [
+      {
+        title: "number one girl",
+      },
+      {
+        title: "3am",
+      },
+      {
+        title: "two years",
+      },
+      {
+        title: "toxic till the end",
+      },
+      {
+        title: "drinks or coffee",
+      },
+      {
+        title: "APT.",
+      },
+      {
+        title: "gameboy",
+      },
+      {
+        title: "stay a little longer",
+      },
+      {
+        title: "not the same",
+      },
+      {
+        title: "call it the end",
+      },
+      {
+        title: "too bad for us",
+      },
+      {
+        title: "dance all night",
+      },
+    ],
+    src: "rosie.png",
+  },
+  {
+    title: "Scarlet",
+    tracks: [
+      {
+        title: "Paint The Town Red",
+      },
+      {
+        title: "Demons",
+      },
+      {
+        title: "Wet Vagina",
+      },
+      {
+        title: "F**k The Girls (FTG)",
+      },
+      {
+        title: "Ouchies",
+      },
+      {
+        title: "97",
+      },
+      {
+        title: "Gun",
+      },
+      {
+        title: "Go Off",
+      },
+      {
+        title: "Shutcho",
+      },
+      {
+        title: "Agora Hills",
+      },
+      {
+        title: "Can't Wait",
+      },
+      {
+        title: "Often",
+      },
+      {
+        title: "Love Life",
+      },
+      {
+        title: "Skull And Bones",
+      },
+      {
+        title: "Attention",
+      },
+      {
+        title: "Balut",
+      },
+      {
+        title: "WYM Freestyle",
+      },
+    ],
+    src: "Scarlet.png",
+  },
+  {
+    title: "Short n' Sweet",
+    tracks: [
+      {
+        title: "Taste",
+      },
+      {
+        title: "Please Please Please",
+      },
+      {
+        title: "Good Graces",
+      },
+      {
+        title: "Sharpest Tool",
+      },
+      {
+        title: "Coincidence",
+      },
+      {
+        title: "Bed Chem",
+      },
+      {
+        title: "Espresso",
+      },
+      {
+        title: "Dumb & Poetic",
+      },
+      {
+        title: "Slim Pickins",
+      },
+      {
+        title: "Juno",
+      },
+      {
+        title: "Lie To Girls",
+      },
+      {
+        title: "Don’t Smile",
+      },
+    ],
+    src: "Short n' Sweet.png",
+  },
+  {
+    title: "SOS Deluxe: LANA",
+    tracks: [
+      {
+        title: "No More Hiding",
+      },
+      {
+        title: "What Do I Do",
+      },
+      {
+        title: "30 For 30 (feat. Kendrick Lamar)",
+      },
+      {
+        title: "Diamond Boy (DTM)",
+      },
+      {
+        title: "BMF",
+      },
+      {
+        title: "Scorsese Baby Daddy",
+      },
+      {
+        title: "Love Me 4 Me",
+      },
+      {
+        title: "Chill Baby",
+      },
+      {
+        title: "My Turn",
+      },
+      {
+        title: "Crybaby",
+      },
+      {
+        title: "Kitchen",
+      },
+      {
+        title: "Get Behind Me (Interlude)",
+      },
+      {
+        title: "Drive",
+      },
+      {
+        title: "Another Life",
+      },
+      {
+        title: "Saturn",
+      },
+      {
+        title: "SOS",
+      },
+      {
+        title: "Kill Bill",
+      },
+      {
+        title: "Seek & Destroy",
+      },
+      {
+        title: "Low",
+      },
+      {
+        title: "Love Language",
+      },
+      {
+        title: "Blind",
+      },
+      {
+        title: "Used (feat. Don Toliver)",
+      },
+      {
+        title: "Snooze",
+      },
+      {
+        title: "Notice Me",
+      },
+      {
+        title: "Gone Girl",
+      },
+      {
+        title: "Smoking on my Ex Pack",
+      },
+      {
+        title: "Ghost in the Machine (feat. Phoebe Bridgers)",
+      },
+      {
+        title: "F2F",
+      },
+      {
+        title: "Nobody Gets Me",
+      },
+      {
+        title: "Conceited",
+      },
+      {
+        title: "Special",
+      },
+      {
+        title: "Too Late",
+      },
+      {
+        title: "Far",
+      },
+      {
+        title: "Shirt",
+      },
+      {
+        title: "Open Arms (feat. Travis Scott)",
+      },
+      {
+        title: "I Hate U",
+      },
+      {
+        title: "Good Days",
+      },
+      {
+        title: "Forgiveless (feat. Ol' Dirty Bastard)",
+      },
+    ],
+    src: "LANA.png",
+  },
+  {
+    title: "SWAG",
+    tracks: [
+      {
+        title: "ALL I CAN TAKE",
+      },
+      {
+        title: "DAISIES",
+      },
+      {
+        title: "YUKON",
+      },
+      {
+        title: "GO BABY",
+      },
+      {
+        title: "THINGS YOU DO",
+      },
+      {
+        title: "BUTTERFLIES",
+      },
+      {
+        title: "WAY IT IS",
+      },
+      {
+        title: "FIRST PLACE",
+      },
+      {
+        title: "SOULFUL",
+      },
+      {
+        title: "WALKING AWAY",
+      },
+      {
+        title: "GLORY VOICE MEMO",
+      },
+      {
+        title: "DEVOTION",
+      },
+      {
+        title: "DADZ LOVE",
+      },
+      {
+        title: "THERAPY SESSION",
+      },
+      {
+        title: "SWEET SPOT",
+      },
+      {
+        title: "STANDING ON BUSINESS",
+      },
+      {
+        title: "405",
+      },
+      {
+        title: "SWAG",
+      },
+      {
+        title: "ZUMA HOUSE",
+      },
+      {
+        title: "TOO LONG",
+      },
+      {
+        title: "FORGIVENESS",
+      },
+    ],
+    src: "SWAG.png",
+  },
+  {
+    title: "The Secret of Us (Deluxe)",
+    tracks: [
+      {
+        title: "Felt Good About You",
+      },
+      {
+        title: "Risk",
+      },
+      {
+        title: "Blowing Smoke",
+      },
+      {
+        title: "I Love You, I'm Sorry",
+      },
+      {
+        title: "us. (feat. Taylor Swift)",
+      },
+      {
+        title: "Let It Happen",
+      },
+      {
+        title: "Tough Love",
+      },
+      {
+        title: "I Knew It, I Know You",
+      },
+      {
+        title: "Gave You I Gave You I",
+      },
+      {
+        title: "Normal Thing",
+      },
+      {
+        title: "Good Luck Charlie",
+      },
+      {
+        title: "Free Now",
+      },
+      {
+        title: "Close To You",
+      },
+      {
+        title: "Cool",
+      },
+      {
+        title: "That’s So True",
+      },
+      {
+        title: "I Told You Things",
+      },
+      {
+        title: "Packing It Up",
+      },
+      {
+        title: "I Love You, I'm Sorry (Live From Vevo)",
+      },
+      {
+        title: "I Knew It, I Know You (Live From Vevo)",
+      },
+      {
+        title: "Free Now (Live From Vevo)",
+      },
+    ],
+    src: "The Secret of Us (Deluxe).png",
   },
 ];
